04a426f9188bef5c31aae7ccfa10083fb05319c3
AetherFabian <fabian_galarza@hotmail.com>
2023-03-06 10:42:24 -0600

    feat: use students as main service

--- students.router.js
+++ students.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
-let database = require('./database').students;
+const database = require('./database');
 
 function runQueries(req, method) {
   const { username, password, email, name } = req.body;
@@ -24,6 +25,8 @@ function runQueries(req, method) {
       database = database.filter(user => user.id != id)
       return user;
     },
+    register: () => database.push(req.body),
+    login: () => database.find(user => user.username == username),
   };
   return queries[method]();
 }
@@ -67,5 +70,33 @@ router.delete('/:id', (req, res, next) => {
   };
   return res.send(user);
 });
+
+router.post('/register', (req, res, next) => {
+  req.body.password = bcrypt.hashSync(req.body.password, 10);
+  
+  const user = runQueries(req, 'register');
+  
+  if (!user) {
+    return next(new Error('No user registred'))
+  };
+  return res.send({user: user.username, message: 'User created successfully'});
+})
+
+router.post('/login', (req, res, next) => {
+  const { password } = req.body;
+
+  const user = runQueries(req, 'login');
+  if (!user) {
+    return next(new Error('No user found'))
+  };
+  
+  if (bcrypt.compareSync(password, user.password)) {
+    return res.send({
+      user: user.username, message: 'User logged in successfully'
+    });
+  } else {
+    return next(new Error('Incorrect password'))
+  }
+})
 
 module.exports = router;
